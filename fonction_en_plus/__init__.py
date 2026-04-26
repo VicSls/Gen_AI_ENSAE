a0ed28687b3a65739f5ad4b8f080614c3bda4422
VicSls <v.soulas@gmail.com>
2026-04-26 12:08:17 +0200

    8th

--- fonction_en_plus/__init__.py
+++ fonction_en_plus/__init__.py
@@ -1,1 +1,4 @@
-from modules.callbacks.ImagesCallback    import ImagesCallback
+from fonction_en_plus.ImagesCallback    import ImagesCallback
+from fonction_en_plus.ImagesCallback_V2    import ImagesCallback_V2
+from fonction_en_plus.DCGAN    import DCGAN
+from fonction_en_plus.WGANGP    import WGANGP
